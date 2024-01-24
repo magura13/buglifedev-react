068ea0088662285afc2ead6a660666d4e7549e95
Samuel Maia Salvador de Assis <samuelufop121035@gmail.com>
2024-01-24 18:55:51 -0300

    adicionado "formatador de data"
    post estilizado

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import formatDateTime from '../shared/dateFormatter';
 
 const Post = ({ data }) => {
+
+  const formattedDate = formatDateTime(data.createdAt)
+  console.log(formattedDate)
+
   return (
     <div className="rounded overflow-hidden shadow-lg p-4 mb-6 text-left">
       <div className="display: flex">
-        <p className="text-gray-700 text-xs mr-0.5">Created by:</p>
-        <p className="text-gray-700 font-bold text-xs font-semibold">Titico Lelis el matador</p>
+        <p className="text-gray-700 text-xs mr-0.5">criado por:</p>
+        <p className="text-custom-blue font-bold text-xs font-semibold mr-0.5">Titico Lelis el matador</p>
+        <p className="text-gray-700 text-xs mr-0.5">postado em</p>
+        <p className="text-custom-blue font-bold text-xs font-semibold">{formattedDate}</p>
       </div>    
-      <h3 className="font-bold text-custom-blue text-xl mb-2 ">{data.content.title}</h3>
-      <p className="text-gray-700 text-base">{data.content.message}</p>
+      <h3 className="font-bold text-custom-blue text-xl my-1 ">{data.content.title}</h3>
+      <p className="text-gray-700 text-sm">{data.content.message}</p>
       <p className="text-gray-700 text-base">{data.comments.message}</p>
     </div>
   );
---
+++ src/shared/dateFormatter.js
@@ -0,0 +1,16 @@
+
+const formatDateTime = (date) => {
+    const newDate = new Date(date)
+    const year = newDate.getFullYear();
+    const month = String(newDate.getMonth() + 1).padStart(2, '0');
+    const day = String(newDate.getDate()).padStart(2, '0');
+    const formattedDate = `${year}/${month}/${day}`;
+
+    const hours = String(newDate.getHours()).padStart(2, '0');
+    const minutes = String(newDate.getMinutes()).padStart(2, '0');
+    const final = formattedDate + " as " + hours + ":" + minutes
+    return final
+}
+
+
+module.exports = formatDateTime
