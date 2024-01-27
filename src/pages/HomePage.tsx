933e44de827e67de6271ae3009b4c105857f1a32
Samuel Maia Salvador de Assis <samuelufop121035@gmail.com>
2024-01-27 09:50:49 -0300

    adjusting dimensions

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,11 +5,11 @@ import NewsFeed from '../components/NewsFeed.tsx';
 const HomePage: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
-    <div className="grid md:grid-cols-3 gap-4">
-      <div className="md:col-span-2">
+    <div className="grid md:grid-cols-3 ">
+      <div className="md:col-span-2 mx-5 pl-40">
         <Feed />
       </div>
-      <div className="md:col-span-1">
+      <div className="md:col-span-1 mx-5 pr-40">
         <NewsFeed />
       </div>
     </div>
