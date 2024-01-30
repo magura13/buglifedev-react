1c2548e10dcf4b3b50251afb4a33062433856430
Samuel <samuelufop121035@gmail.com>
2024-01-30 08:09:54 -0300

    getting access token dinamically

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API_URL = 'https://api-typescript-express.onrender.com';
-const API_BEARER_TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2NTlhODNkY2Q3N2NkZTIxNDA5ODc5N2MiLCJpYXQiOjE3MDY0NDE5NzYsImV4cCI6MTcwNjUyODM3Nn0.wCHLKpoxO3x9-6GjAsWickD_ezRgp7rfTcSsYLWY5ks';
+const API_BEARER_TOKEN = localStorage.getItem('accessToken')
+  
 
 const api = axios.create({
   baseURL: API_URL,
