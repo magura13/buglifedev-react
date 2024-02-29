7af2f7a6cee56253738eecacb40e2bb1eaa08c07
Samuel Maia Salvador de Assis <samuelufop121035@gmail.com>
2024-02-29 06:25:57 -0300

    s

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -14,7 +14,7 @@ const Post = ({ data }) => {
   const userName = localStorage.getItem('userName');
   const userId = localStorage.getItem('userId')?.toString();
   const [comments, setComments] = useState(data.comments);
-  const { sendLike,isLoading,error } = useCreateLike();
+  const { sendLike, isLoading, error } = useCreateLike();
 
 
   const addNewComment = (newComment) => {
@@ -25,12 +25,12 @@ const Post = ({ data }) => {
     try {
 
       const forumPostId = data?._id
-      const likeData:LikeData = {forumPostId,userId}
+      const likeData: LikeData = { forumPostId, userId }
       await sendLike(likeData)
       toast.success('Post curtido :)', { autoClose: 1000 });
 
-    } catch(err) {
-      const filteredError = ErrorFilter.shapingResponse(err.response.status); 
+    } catch (err) {
+      const filteredError = ErrorFilter.shapingResponse(err.response.status);
       toast.info(filteredError);
 
     }
@@ -38,49 +38,50 @@ const Post = ({ data }) => {
 
   const handleCommentClick = () => {
     setShowComments(!showComments);
-  };
 
-  return (
-    <div className="rounded overflow-hidden shadow-lg p-4 mb-6 text-left flex flex-col">
-      <div className="display: flex">
-        <p className="text-gray-700 text-xs mr-0.5">criado por:</p>
-        <p className="text-custom-blue text-xs font-semibold mr-0.5">
-          {data.userName}
-        </p>
-        <p className="text-gray-700 text-xs mr-0.5">postado em</p>
-        <p className="text-custom-blue text-xs font-semibold">
-          {formattedDate}
-        </p>
-      </div>
-      <h3 className="font-bold text-custom-blue text-xl my-1 ">
-        {data.content.title}
-      </h3>
-      <p className="text-gray-700 text-sm">{data.content.message}</p>
-      <div className="display: flex justify-between mt-2">
-        <p 
-        onClick={handleLike}
-        className="text-red-500 cursor-pointer">❤️ Like</p>
-        <div className="display: flex justify-end">
-          <p
-            className="text-gray-700 text-sm mr-1  hover:underline hover:text-custom-blue cursor-pointer"
-            onClick={handleCommentClick}
-          >
-            {data.comments.length} comentários
+
+    return (
+      <div className="rounded overflow-hidden shadow-lg p-4 mb-6 text-left flex flex-col">
+        <div className="display: flex">
+          <p className="text-gray-700 text-xs mr-0.5">criado por:</p>
+          <p className="text-custom-blue text-xs font-semibold mr-0.5">
+            {data.userName}
+          </p>
+          <p className="text-gray-700 text-xs mr-0.5">postado em</p>
+          <p className="text-custom-blue text-xs font-semibold">
+            {formattedDate}
           </p>
         </div>
+        <h3 className="font-bold text-custom-blue text-xl my-1 ">
+          {data.content.title}
+        </h3>
+        <p className="text-gray-700 text-sm">{data.content.message}</p>
+        <div className="display: flex justify-between mt-2">
+          <p
+            onClick={handleLike}
+            className="text-red-500 cursor-pointer">❤️ Like</p>
+          <div className="display: flex justify-end">
+            <p
+              className="text-gray-700 text-sm mr-1  hover:underline hover:text-custom-blue cursor-pointer"
+              onClick={handleCommentClick}
+            >
+              {data.comments.length} comentários
+            </p>
+          </div>
+        </div>
+        {showComments &&
+          comments.map((comment, forumPostId) => (
+            <Comments key={comment.id} data={comment} forumPostId={data._id} />
+          ))}
+        <CommentForm
+          postId={data._id}
+          userId={userId}
+          userName={userName}
+          onCommentAdded={addNewComment}
+        />
       </div>
-      {showComments &&
-        comments.map((comment, forumPostId) => (
-          <Comments key={comment.id} data={comment} forumPostId={data._id} />
-        ))}
-      <CommentForm
-        postId={data._id}
-        userId={userId}
-        userName={userName}
-        onCommentAdded={addNewComment}
-      />
-    </div>
-  );
+    );
+  }
 };
 
 export default Post;
